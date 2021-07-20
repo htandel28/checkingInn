61f2c419a70ca06d9305f859807e4b2e8d723424
Hiya Tandel <hiyatandel@gmail.com>
2021-07-20 14:30:29 -0600

    I added a label.

---
+++ checkingInn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checkingInn
+//
+//  Created by  Scholar on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
